45c59c07e3a4ea0f4ecfaf3d468dcd3324046a93
SoftwareMickey <119460540+SoftwareMickey@users.noreply.github.com>
2024-10-16 19:08:10 +0300

    firebase error handling

--- controller/apps.py
+++ controller/apps.py
@@ -1,10 +1,10 @@
-import json
-import os
+# import json
+# import os
 
 from django.apps import AppConfig
 
-import firebase_admin
-from firebase_admin import credentials
+# import firebase_admin
+# from firebase_admin import credentials
 
 class ControllersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -13,19 +13,19 @@ class ControllersConfig(AppConfig):
     # name = 'firebaseapp'
     # Get the current directory and construct the file path
         
-    def ready(self):
+    # def ready(self):
         
-        current_dir = os.path.dirname(__file__)
-        file_path = os.path.join(current_dir, './vector-key.json')
+    #     current_dir = os.path.dirname(__file__)
+    #     file_path = os.path.join(current_dir, './vector-key.json')
 
-        # Read the service account key JSON file
-        with open(file_path, 'r') as f:
-            service_account_info = json.load(f)
+    #     # Read the service account key JSON file
+    #     with open(file_path, 'r') as f:
+    #         service_account_info = json.load(f)
 
-        # Initialize Firebase Admin SDK with the loaded credentials
-        cred = credentials.Certificate(service_account_info)
-        # Initialize Firebase app here to ensure it's only initialized once
-        if not firebase_admin._apps:
-            firebase_admin.initialize_app(cred, {
-                'storageBucket': 'vector-technologies-16b3f.appspot.com'
-            })
+    #     # Initialize Firebase Admin SDK with the loaded credentials
+    #     cred = credentials.Certificate(service_account_info)
+    #     # Initialize Firebase app here to ensure it's only initialized once
+    #     if not firebase_admin._apps:
+    #         firebase_admin.initialize_app(cred, {
+    #             'storageBucket': 'vector-technologies-16b3f.appspot.com'
+    #         })
--- models/firebase.py
+++ models/firebase.py
@@ -1,12 +1,12 @@
-import firebase_admin
-from firebase_admin import credentials
+# import firebase_admin
+# from firebase_admin import credentials
 
-print('-------------------------- FIREBASE CONFIGURATION --------------------------------------------------')
+# print('-------------------------- FIREBASE CONFIGURATION --------------------------------------------------')
 
-# Path to your service account key file
-cred = credentials.Certificate('../vector-key.json')
+# # Path to your service account key file
+# cred = credentials.Certificate('../vector-key.json')
 
-# Initialize the Firebase app
-firebase_admin.initialize_app(cred, {
-    'storageBucket' : 'vector-technologies-16b3f.appspot.com'
-})
+# # Initialize the Firebase app
+# firebase_admin.initialize_app(cred, {
+#     'storageBucket' : 'vector-technologies-16b3f.appspot.com'
+# })
